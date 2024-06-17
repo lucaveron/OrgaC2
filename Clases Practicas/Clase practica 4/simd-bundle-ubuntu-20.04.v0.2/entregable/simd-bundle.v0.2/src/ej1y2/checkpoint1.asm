125f2fe06a17c3c489ed2a604460174d11991ba1
Luca Verón <lucaver@hotmail.com>
2024-06-17 19:38:22 -0300

    agregado clases practicas

---
+++ Clases Practicas/Clase practica 4/simd-bundle-ubuntu-20.04.v0.2/entregable/simd-bundle.v0.2/src/ej1y2/checkpoint1.asm
@@ -0,0 +1,10 @@
+
+section .text
+
+global dot_product_asm
+
+; uint32_t dot_product_asm(uint16_t *p, uint16_t *q, uint32_t length);
+; implementacion simd de producto punto
+
+dot_product_asm:
+	ret
---
+++ Clases Practicas/Clase practica 4/simd-bundle-ubuntu-20.04.v0.2/entregable/simd-bundle.v0.2/src/ej1y2/checkpoint2.asm
@@ -0,0 +1,10 @@
+
+section .text
+
+global four_of_a_kind_asm
+
+; uint32_t four_of_a_kind_asm(card_t *hands, uint32_t n);
+
+four_of_a_kind_asm:
+	ret
+
